dbd1c02f3f477e8f4f51804a086c4c2281d8ae9a
Santosh Kumar verma <santosh.verma@itgrids.com>
2018-07-16 11:31:41 +0530

    updated by santosh

--- HelloWorldSoapProject/src/main/java/com/test/publisher/Publish.java
+++ HelloWorldSoapProject/src/main/java/com/test/publisher/Publish.java
@@ -8,7 +8,7 @@ import com.test.service.impl.ProductService;
 public class Publish {
 
 	public static void main(String[] args) {
-		//This is publisher class,which will expose or publish soap web service
+		//This is publisher class,which will expose or publish soap web service to web or internet
 		Endpoint.publish("http://localhost:8071/product", new ProductService());
 		Endpoint.publish("http://localhost:8072/employee", new EmployeeService());
 		System.out.println("Done");
